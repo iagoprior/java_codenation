9341f2816e6d6ca46fc7cb9d8395a2d7d58c98f8
Iago Prior <iagoi.a.s.p_@hotmail.com>
2020-07-16 20:52:30 -0300

    adicionando desafio 1 da semana 1 com 2 resoluções diferentes

---
+++ semana_1/desafio_1/java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication_2.java
@@ -0,0 +1,52 @@
+package br.com.codenation.desafioexe;
+
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+
+public class DesafioApplication {
+
+
+
+
+    public static List<Integer> fibonacci() {
+
+        List<Integer> fibo = new ArrayList<>();
+
+        fibo.add(0,0);
+
+        fibo.add(1,1);
+
+        for(int i = 2; i<15; i++){
+
+            fibo.add(i,fibo.get(i - 2) + fibo.get(i - 1));
+
+        }
+
+        return new ArrayList<>(fibo);
+
+
+
+    }
+
+
+
+    public static Boolean isFibonacci(Integer a){
+
+
+
+        return fibonacci().contains(a);
+
+
+    }
+
+
+
+
+}
+
+

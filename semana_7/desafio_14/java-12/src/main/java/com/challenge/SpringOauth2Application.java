0c22730f59dd7a1545cddeb1f9d3ab568a03734c
Iago Prior <iagoi.a.s.p_@hotmail.com>
2020-07-16 23:47:31 -0300

    adicionando desafio 14 da semana 6

---
+++ semana_7/desafio_14/java-12/src/main/java/com/challenge/SpringOauth2Application.java
@@ -0,0 +1,21 @@
+package com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2Application.class, args);
+    }
+
+}
